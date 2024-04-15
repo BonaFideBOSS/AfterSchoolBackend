c6bda29a418e2c4a012f86dff7e4c212b5eb3366
BONAFIDE BOSS <contact@almir.info>
2024-04-15 11:29:24 +0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 app.set("trust proxy", true);
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 app.use(express.json());
 app.use(cors());
 
