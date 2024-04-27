e51f45ca51f3df4954645950634620fe080de0be
BONAFIDE BOSS <contact@almir.info>
2024-04-28 00:23:36 +0400

    testing cors

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,6 @@
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log("Request origin", origin);
     const allowedOrigins = process.env.allowedOrigins;
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
